aae39bef2cd0d6876cbf0e9ebcbd7f547858c6e1
Amarnath-361 <amarnathshindhur@gmail.com>
2022-07-24 14:31:23 -0800

    this is second commit -with side bar and chat window INCOMPLETE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import './App.css';
+import Chat from './Chat';
+import './Sidebar.js'
+import Sidebar from './Sidebar.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>lets build mern whatsapp</h1>
+      <div className="app__body">
+      <Sidebar/>
+      <Chat/>
+      </div>
+
+
+      {/* */}
     </div>
   );
 }
---
+++ src/Chat.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Chat.css"
+
+function Chat() {
+  return (
+    <div><h1>this is a chat section</h1></div>
+  )
+}
+
+export default Chat
