531a0877a19edf458ab040490a98710f1b597a2f
Amarnath-361 <amarnathshindhur@gmail.com>
2022-07-24 19:16:32 -0800

    third commit sidebar completed ...starting with chat component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,29 +1,42 @@
-import React from 'react'
-import './Sidebar.css'
-import ChatIcon from '@mui/icons-material/Chat';
-import DonutLargeIcon from '@mui/icons-material/DonutLarge';
-import { IconButton } from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import React from "react";
+import "./Sidebar.css";
+import ChatIcon from "@mui/icons-material/Chat";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import { Avatar, IconButton } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { SearchOutlined } from "@mui/icons-material";
+import SidebarChat from "./SidebarChat";
 function Sidebar() {
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       <div className="sidebar__header">
+        <Avatar src="https://images.unsplash.com/photo-1542596768-5d1d21f1cf98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
         <div className="side__headerRight">
-          
           <IconButton>
-          <DonutLargeIcon/>
+            <DonutLargeIcon />
           </IconButton>
           <IconButton>
             <ChatIcon />
           </IconButton>
           <IconButton>
-            <MoreVertIcon/>
+            <MoreVertIcon />
           </IconButton>
-
         </div>
       </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start a new chat" type="text" />
+        </div>
       </div>
-  )
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
